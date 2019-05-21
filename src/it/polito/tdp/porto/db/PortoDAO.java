fad7ae6c3fc3ebbeaff3f3422600d087836a67c3
zierp <zierp@172.22.42.28>
2019-05-21 19:06:29 +0200

    v 1.0

--- src/it/polito/tdp/porto/db/PortoDAO.java
+++ src/it/polito/tdp/porto/db/PortoDAO.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import it.polito.tdp.porto.model.Accoppiamenti;
 import it.polito.tdp.porto.model.Author;
 import it.polito.tdp.porto.model.Paper;
 
@@ -38,6 +41,38 @@ public class PortoDAO {
 		}
 	}
 
+	/**
+	 * Ottengo tutti gli autori
+	 * @param id
+	 * @return
+	 */
+	public List<Author> getAllAutori() {
+
+		final String sql = "SELECT * FROM author";
+		List<Author> res = new ArrayList<>();
+
+		
+		try {
+			Connection conn = DBConnect.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			
+
+			ResultSet rs = st.executeQuery();
+
+			while (rs.next()) {
+
+				Author autore = new Author(rs.getInt("id"), rs.getString("lastname"), rs.getString("firstname"));
+				res.add(autore);
+			}
+
+			return res;
+
+		} catch (SQLException e) {
+			// e.printStackTrace();
+			throw new RuntimeException("Errore Db",e);
+		}
+	}
+	
 	/*
 	 * Dato l'id ottengo l'articolo.
 	 */
@@ -59,6 +94,31 @@ public class PortoDAO {
 			}
 
 			return null;
+
+		} catch (SQLException e) {
+			 e.printStackTrace();
+			throw new RuntimeException("Errore Db");
+		}
+	}
+	
+	public List<Accoppiamenti> getArchiGrafo(){
+		
+		final String sql = "SELECT c1.authorid AS id1, c2.authorid AS id2 FROM creator c1, creator c2 WHERE c1.eprintid=c2.eprintid AND c1.authorid>c2.authorid";
+		List<Accoppiamenti> res = new ArrayList<>();
+		
+		try {
+			Connection conn = DBConnect.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			
+
+			ResultSet rs = st.executeQuery();
+
+			while (rs.next()) {
+				
+				
+			}
+
+			return res;
 
 		} catch (SQLException e) {
 			 e.printStackTrace();
--- src/it/polito/tdp/porto/model/Model.java
+++ src/it/polito/tdp/porto/model/Model.java
@@ -1,5 +1,14 @@
 package it.polito.tdp.porto.model;
 
+import java.util.List;
+
+import it.polito.tdp.porto.db.PortoDAO;
+
 public class Model {
+	
+	public List<Author> getAllAutori(){
+		PortoDAO dao = new PortoDAO();
+		return dao.getAllAutori();
+	}
 
 }
